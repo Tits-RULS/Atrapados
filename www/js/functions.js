d7326828ec4028c6401f4d5adbf81ceccbff2233
alumno <alumno@U109021>
2015-10-22 12:34:28 +0200

    funciona para una pregunta, no sobrescribe tras esto

--- www/js/functions.js
+++ www/js/functions.js
@@ -5,8 +5,20 @@ function next(){
 	//$("body").append(pageDiv);
 	//question_page.load(i)
 	alert("antes de window");
-	window.location.replace("question.html");
+	document.location.href = "#question-page";
 	alert("tras window");
-	question_page.load(i);
+	load(i);
 	alert("tras el load");
+}
+
+function load(i){
+	alert("load");
+	$("#question").text(tests.test[i].question);
+	alert("antes del each");
+	$("button[id|='question-button']").each(
+ 			function(index) {     				
+ 				$(this).text(tests.test[i].resp[index]);
+ 				alert(tests.test[i].resp[index]);
+		    }
+ 	);
 }
--- www/js/questions.js
+++ www/js/questions.js
@@ -35,44 +35,5 @@ var tests = {
 	    		correct: "0",
 	       }
 	 ]
-};
-
-var question_page={
-		create : function(){
-			var pageDiv=$('<div data-role="page" id="question-page"></div>');
-			//var pageDiv=$('<div id="question-page"></div>');
-			var headerDiv=
-				'<div data-role="header" data-position="fixed" >'+
-					'<h1 style="margin-left:0;margin-right:0;white-space: nowrap;overflow: visible;">Pregunta</h1>'+
-				'</div>';
-			var contentDiv=
-				'<div data-role="content">'+
-					'<form id="question-form">'+
-						'<h2 name="question" id="question"></h2>'+
-						'<button name="question-buttonA" id="question-buttonA" value="0"></button>'+
-						'<label for="question-buttonA" id="question-labelA"></label>'+
-						'<button name="question-buttonB" id="question-buttonB" value="1"></button>'+
-						'<label for="question-buttonB" id="question-labelB"></label>'+
-						'<button name="question-buttonC" id="question-buttonC" value="2"></button>'+
-						'<label for="question-buttonC" id="question-labelC"></label>'+
-						'<button name="question-buttonD" id="question-buttonD" value="3"></button>'+
-						'<label for="question-buttonD" id="question-labelD"></label>'+
-					'</form>';
-			pageDiv.append(headerDiv,contentDiv); 
-			return(pageDiv);
-		},
-		 
-		load : function(i){
-			alert("load");
-			$("#question").text("QUESTION: "+tests.test[i].question);
-			alert("antes del each");
-			$("label[id|='question-label']").each(
-	     			function(index) {     				
-	     				$(this).text(tests.test[i].resp[index]);
-	     				alert(tests.test[i].resp[index]);
-	    		    }
-	     	);
-		}
-			
-
 }
+
